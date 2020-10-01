6635f9c5b25271b7364801ad2328a9b7ef21f2f1
Нестерчук Григорий Анатольевич <st021213@student.spbu.ru>
2020-10-01 10:13:25 +0300

    Some code added

---
+++ 2020.10.01-Lesson-3/Task1/Source.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+
+using namespace std;
+
+void swap(int &a, int &b)
+{
+	int c = a;
+	a = b;
+	b = c;
+}
+
+void increment1(int i)
+{
+	i++;
+}
+
+void increment2(int& i)
+{
+	i++;
+}
+
+int main()
+{
+	int a = 0;
+	cout << "a = " << a << endl;
+	increment1(a);
+	cout << "inrement 1: " << a << endl;
+
+	increment2(a);
+	cout << "inrement 2: " << a << endl;
+
+	increment2(a);
+	cout << "inrement 2: " << a << endl;
+
+	increment1(a);
+	cout << "inrement 1: " << a << endl;
+
+	increment2(a);
+	cout << "inrement 2: " << a << endl;
+
+	increment2(a);
+	cout << "inrement 2: " << a << endl;
+
+	int c = 19;
+	cout << "a = " << a << "\tc = " << c << endl;
+	swap(a, c);
+	cout << "a = " << a << "\tc = " << c << endl;
+
+	return EXIT_SUCCESS;
+}
